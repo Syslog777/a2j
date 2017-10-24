fd28f56a97f57669f70790f532ad1bf084f42fe8
BitCoder <18704463+BitCoder444@users.noreply.github.com>
2017-10-24 13:37:41 -0500

    Update a2j.sh

--- a2j.sh
+++ a2j.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Usage sh ./a2j-master path/tp/adroidApp.apk /ouput/directory -options
 # $1 is the apk, $2 is the output directory, $3 or more are the options
 #Initialize a2j directory variable
 a2jDir="$(pwd )"
@@ -6,20 +7,20 @@ a2jDir="$(pwd )"
 #Collect the path to the file of type .apk
 apk=$1
 
-#Replace the .apk extension from the $1 string with .jar. 
+#Replace the .apk extension from the $1 string with .jar. This will be the output jar file for dex2jar
 jar=${1%.*}
 jar="$jar.jar"
 
-#Lets prep the dex2jar scripts to be executable
+#Prep the dex2jar scripts to be executable
 find $a2jDir -type f -exec chmod +x {} \;
 
 #The jar file does not exist yet, but we will get to that soon. Lets go to the dex2jar folder
 cd ./dex2jar-2.0
 
 #Convert the apk to jar
-sh d2j-dex2jar.sh -f $apk
+sh d2j-dex2jar.sh -f $apk -o $jar
 
-#Collect the arguments, but skip the first two args
+#Collect the arguments for jd-cli
 collective=("")
 counter=1 
 for i in ${@[@]}
@@ -33,9 +34,6 @@ do
    fi
 done
 
-#Go to jd-cli
-cd ..
-cd ./jd-cmd-master
-
 #Convert the jar to java files
-java -jar jd-cli.jar $collective
+java -jar "$a2jDir/jd-cli-0.9.1.Final-dist/jd-cli.jar" -od $2 $collective $jar
+
